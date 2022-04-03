34ff4bce31bf24a21599ec11f8782721760d6cf4
SirCrewete <gimenocrew1996@gmail.com>
2022-04-03 12:14:41 +0200

    login and register ok

--- app/src/main/java/com/example/atipik/MainActivity.kt
+++ app/src/main/java/com/example/atipik/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.atipik
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -14,6 +15,60 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Analitics Eventos
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "FirebaseOk")
+        analytics.logEvent("InitScreen", bundle)
+
+        setup()
     }
+
+    //Intent para pasar a actiivty Registrarse
+    fun goRegistrarse (view: View) {
+        val btnRegistrarse = findViewById<Button>(R.id.btnRegistro);
+        val intent = Intent(this, RegistroActivity::class.java)
+        startActivity(intent)
+    }
+
+    //Intent goHome
+    fun goGome (view: View) {
+        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
+        val intent = Intent(this, menuActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun setup() {
+        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
+        val btnRegistro = findViewById<Button>(R.id.btnRegistro)
+        var textEmail = findViewById<EditText>(R.id.txtEmail)
+        var textPassword = findViewById<EditText>(R.id.textPassword)
+
+
+        btnRegistro.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java);
+            startActivity(intent)
+        }
+
+        btnIniciarSesion.setOnClickListener {
+            if (textEmail.text.isNotBlank() && textPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(textEmail.text.toString(),
+                        textPassword.text.toString()).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val toast = Toast.makeText(applicationContext, "Registro ok", Toast.LENGTH_SHORT)
+                                val intent = Intent(this, menuActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                val toast = Toast.makeText(applicationContext, "FAIL", Toast.LENGTH_SHORT)
+                            }
+                    }
+            }
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/example/atipik/RegistroActivity.kt
+++ app/src/main/java/com/example/atipik/RegistroActivity.kt
@@ -1,14 +1,40 @@
 package com.example.atipik
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        registrarse()
+    }
+
+    private fun registrarse() {
+        var emailText = findViewById<EditText>(R.id.email)
+        var passwordText = findViewById<EditText>(R.id.password)
+        var btnRegistrarse = findViewById<Button>(R.id.registrarse)
+
+        btnRegistrarse.setOnClickListener {
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(emailText.text.toString(),
+                    passwordText.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val toast = Toast.makeText(applicationContext, "Registro ok", Toast.LENGTH_LONG)
+                            val intent = Intent(this, menuActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            val toast = Toast.makeText(applicationContext, "FAIL", Toast.LENGTH_LONG)
+                        }
+                }
+        }
     }
 }
